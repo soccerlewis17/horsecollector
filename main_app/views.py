203d6c7f9355d4fdb0acaa03715ef57772288362
soccerlewis17 <soccerlewis17@gmail.com>
2022-12-21 16:48:06 -0500

    Bootstrap style on index page

--- main_app/views.py
+++ main_app/views.py
@@ -19,7 +19,7 @@ horses = [
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
