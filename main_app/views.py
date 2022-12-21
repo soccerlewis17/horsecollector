5d1787b93d54a276114a0a589dc5b8c07f40a883
soccerlewis17 <soccerlewis17@gmail.com>
2022-12-21 14:40:53 -0500

    About and index page on horsecollector

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Home Page</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
+def horses_index(request):
+    return render(request, 'horses/index.html')
