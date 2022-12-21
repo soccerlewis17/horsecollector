e9044a1c1cee212efb8524e28a1778fa95808630
soccerlewis17 <soccerlewis17@gmail.com>
2022-12-21 14:59:53 -0500

    Horses list on index page

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
+class Horse:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+horses = [
+  Horse('Vroom', 'Mustang', 'very fast but not great with kids', 4),
+  Horse('Big Ben', 'Clydesdale', 'towers over everything but very loveable personality', 2),
+  Horse('Aspen', 'American Quarter Horse', 'just born a few weeks ago', 0)
+]
+
+
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Home Page</h1>')
@@ -10,4 +25,4 @@ def about(request):
     return render(request, 'about.html')
 
 def horses_index(request):
-    return render(request, 'horses/index.html')
+    return render(request, 'horses/index.html', {'horses': horses})
